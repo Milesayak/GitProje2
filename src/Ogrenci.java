3d9ffa1d6a5a5106a2a8a8b9287601a3e6b3f7db
miles <milesayak1996@hotmail.com>
2022-07-13 17:31:58 +0300

    Ogrenci Classi ikinci bolum yapildi

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,6 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("Merhaba Dunya");
+        System.out.println("Merhaba Dunya 2");
+        System.out.println("Merhaba Dunay 3");
     }
 }
